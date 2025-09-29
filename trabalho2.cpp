2b486ac581ee3fb6e01e7bd3dd73bd71288384af
Miguel wheeler <dinossaurotop10@gmail.com>
2025-09-29 12:26:36 +0100

    Add files via upload

---
+++ trabalho2.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+int main (){
+int p1,p2,p3,p4,media;
+std::cout << "porfavor escreva um numero";
+std::cin >>p1;
+
+std::cout<< "Porfavor escreva um numero";
+std::cin>>p2;
+
+std::cout<<"porfavor escreva um numero";
+std::cin>>p3;
+
+std::cout<<"porfavor escreva um numero";
+std::cin>>p4;
+
+media = (p1+p2+p3+p4) / 4;
+
+std::cout<<"a media dos numero é" << media << " ."; 
+    
+}
