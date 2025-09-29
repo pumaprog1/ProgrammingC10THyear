dc3cdeeaec0b897951c96990250e2a2f115995e0
Miguel wheeler <dinossaurotop10@gmail.com>
2025-09-29 13:27:16 +0100

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+int main() {
+    int kmI,kmF,GasolinaGasta,resultado;
+    
+    std::cout<< "Kilometros iniciais";
+    std::cin>>kmI;
+    
+    std::cout<< "kilometros finais";
+    std::cin>> kmF;
+    
+    resultado =(GasolinaGasta* 100 ) / (kmF-kmI);
+    
+    std::cout<<"o veiculo gasta"<<resultado<<"litros gastos";
+    
+    
+    
+}
