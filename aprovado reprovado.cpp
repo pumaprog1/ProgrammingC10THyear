ce04e2f24658f73c4159801cb2c81db27e5836de
Miguel wheeler <dinossaurotop10@gmail.com>
2025-10-01 12:27:00 +0100

    Add files via upload

---
+++ aprovado reprovado.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+int main(){
+    int nota;
+    
+std::cout<<"qual a tua nota?";
+std::cin>>nota;
+
+if (nota<9.5)
+std::cout<<"reprovado:)";
+
+
+ if (nota>=9.5)
+std::cout<<"Aprovado:(";
+}
