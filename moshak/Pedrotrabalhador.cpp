0d00c620d815f3bf7ad546833540def6d577e3ec
Miguel wheeler <dinossaurotop10@gmail.com>
2025-11-06 16:43:49 +0000

    Add files via upload

---
+++ moshak/Pedrotrabalhador.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <locale.h>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	srand(time(0));
+	int Altura,Largura,Comprimento,Volume;
+
+	cin >> Altura >> Comprimento >> Largura;
+
+	Volume = Altura * Comprimento * Largura;
+
+	if (Volume >= 50 && Altura >= 3) {
+		cout << 1 <<endl;
+
+
+	}
+	else
+	{
+		cout << 0<<endl;
+	}
+	return 0;
+}
