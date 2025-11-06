0e28a02c82c80652d77bc8deb627355ba94b2842
Miguel wheeler <dinossaurotop10@gmail.com>
2025-11-06 16:26:08 +0000

    Add files via upload

---
+++ arrays/bubble ray.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <locale.h>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	int array[10] = { 15,2,53,4,1,17,5,23,45,19 };
+	int aux1, aux2;
+	do {
+
+		aux1 = 0;
+		for (int i = 0; i < 9; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				aux1++;
+				aux2 = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = aux2;
+
+			}
+
+
+		}
+
+
+
+    } while (aux1 != 0);
+
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << array[i] << "\n";
+	}
+}
